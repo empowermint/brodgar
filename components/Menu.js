286cd3ef5e214a43ed49ae24026ebd01c2234211
James T. Harding <59349643+empowermint@users.noreply.github.com>
2022-09-25 13:13:38 +0100

    Refactor into function component

--- components/Menu.js
+++ components/Menu.js
@@ -1,19 +1,17 @@
 import Link from 'next/link'
 
-export const Menu = () => {
-  render() {
-    return (
-      <>
-        <nav id="main-menu">
-          <ul className="main-menu">
-            <li>
-              <Link href="/">
-                <a className="main-menu__link">Home</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </>
-    )
-  }
+export default function Menu() {
+  return (
+    <>
+      <nav id="main-menu">
+        <ul className="main-menu">
+          <li>
+            <Link href="/">
+              <a className="main-menu__link">Home</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
 }
