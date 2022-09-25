ddd5bb7e45791892b5f54dadee478f7ea0c62cc3
James T. Harding <59349643+empowermint@users.noreply.github.com>
2022-09-25 13:13:51 +0100

    Implement Layout

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Layout from '../components/Layout'
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Brodgar Poetry/Sound Walk</title>
-        <meta name="description" content="A poetry/sound experience for Brodgar in Orkney" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
+    <Layout>
+      <p>Test content</p>
+    </Layout>
   )
 }
