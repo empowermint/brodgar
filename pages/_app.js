934c13e2060262fd9034817ab6dc368b676ca785
James T. Harding <59349643+empowermint@users.noreply.github.com>
2022-10-10 18:23:47 +0100

    Remove Tina

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,8 @@
-import Tina from '../.tina/components/TinaDynamicProvider.js'
 import '../styles/globals.css'
 
 const App = ({ Component, pageProps }) => {
   return (
-    <Tina>
       <Component {...pageProps} />
-    </Tina>
   )
 }
 
