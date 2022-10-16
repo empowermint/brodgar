db672fe894b62d9a32ecb3a8db7b164874069b0a
James T. Harding <59349643+empowermint@users.noreply.github.com>
2022-10-16 16:12:39 +0100

    Create poetry experience page

---
+++ pages/poems.js
@@ -0,0 +1,17 @@
+import Layout from "../components/Layout"
+
+
+export default function Poems() {
+  const meta = {
+    title: "Listen Online",
+    description: "Experience the walk online"
+  }
+
+  return (
+    <Layout meta={meta}>
+      <section>
+        <h1>Section</h1>
+      </section>
+    </Layout>
+  )
+}
