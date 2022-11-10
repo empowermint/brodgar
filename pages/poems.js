e3bb95b14908ce1f74dd13664d322675f2270514
James T. Harding <59349643+empowermint@users.noreply.github.com>
2022-11-10 20:45:55 +0000

    Import and place poems

--- pages/poems.js
+++ pages/poems.js
@@ -1,6 +1,13 @@
 import Layout from "../components/Layout"
 
 import Lapwings from "../poems/deceit-of-lapwings.mdx"
+import Ring from "../poems/at-the-ring-of-brodgar.mdx"
+import Loch from "../poems/down-to-the-loch.mdx"
+import Bumblebee from "../poems/great-yellow-bumblebee.mdx"
+import Birdsong from "../poems/no-more-birdsong.mdx"
+import Peewit from "../poems/peewit.mdx"
+import Stone from "../poems/stone-water-sky.mdx"
+import Curlew from "../poems/alternative-names-for-curlew.mdx"
 
 export default function Poems() {
   const meta = {
@@ -11,8 +18,28 @@ export default function Poems() {
   return (
     <Layout meta={meta}>
       <section>
-        <h1>Section</h1>
+        <Stone />
+      </section>
+      <section>
         <Lapwings />
+      </section>
+      <section>
+        <Bumblebee />
+      </section>
+      <section>
+        <Loch />
+      </section>
+      <section>
+        <Curlew /> 
+      </section>
+      <section>
+        <Birdsong />
+      </section>
+      <section>
+        <Peewit />
+      </section>
+      <section>
+        <Ring /> 
       </section>
     </Layout>
   )
