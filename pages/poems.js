a5488d42344267991d2c88ef3921dc265a9a6b73
James T. Harding <59349643+empowermint@users.noreply.github.com>
2022-11-30 11:51:30 +0000

    Add audio tracks for each poem

--- pages/poems.js
+++ pages/poems.js
@@ -1,5 +1,6 @@
 import Layout from "../components/Layout"
 import Photo from "../components/Photo"
+import AudioPlayer from "../components/AudioPlayer"
 
 import Lapwings from "../poems/deceit-of-lapwings.mdx"
 import Ring from "../poems/at-the-ring-of-brodgar.mdx"
@@ -20,48 +21,56 @@ export default function Poems() {
     <Layout meta={meta} className="poetry-page">
       <section>
         <Photo src="/img/photo s1.jpg" alt="TBC" />
+        <AudioPlayer src="./audio/Rewilding_ Brodgar UMBRELLA_verse 1.mp3" />
         <div className="poem">
           <Stone />
         </div>
       </section>
       <section>
         <Photo src="/img/photo s2.jpg" alt="TBC" />
+        <AudioPlayer src="./audio/Rewilding_ Brodgar UMBRELLA_verse 2.mp3" />
         <div className="poem">
           <Lapwings />
         </div>
       </section>
       <section>
         <Photo src="/img/photo s3.jpg" alt="TBC" />
+        <AudioPlayer src="./audio/Rewilding_ Brodgar UMBRELLA_verse 3.mp3" />
         <div className="poem">
           <Loch />
         </div>
       </section>
       <section>
-      <Photo src="/img/photo s4.jpg" alt="TBC" />
+        <Photo src="/img/photo s4.jpg" alt="TBC" />
+        <AudioPlayer src="./audio/Rewilding_ Brodgar UMBRELLA_verse 4.mp3" />
         <div className="poem">
           <Bumblebee />
         </div>
       </section>
       <section>
         <Photo src="/img/photo s5.jpg" alt="TBC" />
+        <AudioPlayer src="./audio/Rewilding_ Brodgar UMBRELLA_verse 5.mp3" />
         <div className="poem">
           <Peewit />
         </div>
       </section>
       <section>
         <Photo src="/img/photo s6.jpg" alt="TBC" />
+        <AudioPlayer src="./audio/Rewilding_ Brodgar UMBRELLA_verse 6.mp3" />
         <div className="poem">
           <Curlew /> 
         </div>
       </section>
       <section>
         <Photo src="/img/photo s7.jpg" alt="TBC" />
+        <AudioPlayer src="./audio/Rewilding_ Brodgar UMBRELLA_verse 7.mp3" />
         <div className="poem">
           <Birdsong />
         </div>
       </section>
       <section>
         <Photo src="/img/photo s8.jpg" alt="TBC" />
+        <AudioPlayer src="./audio/Rewilding_ Brodgar UMBRELLA_verse 8.mp3" />
         <div className="poem">
           <Ring /> 
         </div>
