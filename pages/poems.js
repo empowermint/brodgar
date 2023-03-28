f3794c8b63236180525c451381182bac030dc5b7
James T. Harding <59349643+empowermint@users.noreply.github.com>
2023-03-28 17:19:01 +0100

    Fix Netlify build error

--- pages/poems.js
+++ pages/poems.js
@@ -4,28 +4,28 @@ import Layout from "../components/Layout"
 import AudioPlayer from "../components/AudioPlayer"
 
 import Stone from "../poems/stone-water-sky.mdx"
-import imgStone from "../public/img/photo s1.jpg"
+import imgStone from "../public/img/photo_s1.jpg"
 
 import Lapwings from "../poems/deceit-of-lapwings.mdx"
-import imgLapwings from "../public/img/photo s2.jpg"
+import imgLapwings from "../public/img/photo_s2.jpg"
 
 import Loch from "../poems/down-to-the-loch.mdx"
-import imgLoch from "../public/img/photo s3.jpg"
+import imgLoch from "../public/img/photo_s3.jpg"
 
 import Bumblebee from "../poems/great-yellow-bumblebee.mdx"
-import imgBumblebee from "../public/img/photo s4.jpg"
+import imgBumblebee from "../public/img/photo_s4.jpg"
 
 import Peewit from "../poems/peewit.mdx"
-import imgPeewit from "../public/img/photo s5.jpg"
+import imgPeewit from "../public/img/photo_s5.jpg"
 
 import Curlew from "../poems/alternative-names-for-curlew.mdx"
-import imgCurlew from "../public/img/photo s6.jpg"
+import imgCurlew from "../public/img/photo_s6.jpg"
 
 import Birdsong from "../poems/no-more-birdsong.mdx"
-import imgBirdsong from "../public/img/photo s7.jpg"
+import imgBirdsong from "../public/img/photo_s7.jpg"
 
 import Ring from "../poems/at-the-ring-of-brodgar.mdx"
-import imgRing from "../public/img/photo s8.jpg"
+import imgRing from "../public/img/photo_s8.jpg"
 
 export default function Poems() {
   const meta = {
