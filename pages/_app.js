4c33d4f00f16642bd0af4a556950e8a5f756c303
James T. Harding <59349643+empowermint@users.noreply.github.com>
2023-04-30 13:39:08 +0100

    Add Google Analytics token

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/global.scss'
+import Script from 'next/script'
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-2NQM3JB3X4" />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-2NQM3JB3X4');
+            `
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
